45d38c1c109408af19fa555fdedbbf8bb7ffe70c
kawtar Elkhoumani <kawtar.elkhoumani@outlook.com>
2024-01-10 13:50:14 -0800

    models

---
+++ models/engine/file_storage.py
@@ -0,0 +1,36 @@
+
+#!/usr/bin/python3
+
+import json
+from models
+
+
+class FileStorage:
+    """
+    Serializes instances to a JSON file and deserializes JSON file to instances
+    """
+    __file_path = "file.json"
+    __objects = {}
+
+    def all(self):
+        return FileStorage.__objetcs
+
+    def new(self, obj):
+        key = type(obj).__name__ + '.' + obj.id
+        FileStorage.__[key] = obj
+
+        def save(self):
+            """
+            serializes FileStorage.__objects
+            """
+            with open(FileStorage.__file_path, 'w+') as f:
+                dictofobjs = {}
+                for key, value in FileStorage.__objects.items():
+                    dictofobj[key] = value.to_dict()
+                json.dump(dictofobjs, f)
+
+            def reload(self):
+                """
+                deserialzes instances got from json file
+                """
+

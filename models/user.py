d3cf4740eb58176efdefa882045edd0900b2c855
kawtar Elkhoumani <kawtar.elkhoumani@outlook.com>
2024-01-12 05:50:43 -0800

    T5

---
+++ models/user.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""
+Class User that inherits from BaseModel
+"""
+
+from models.base_model import BaseModel
+
+class user(BaseModel):
+    """
+    Blueprint for a User object
+    Public Attributes that will use FileStorage in engine
+    folder to mange serialisation and deserialization of user
+    """
+    email = ""
+    password = ""
+    first_name = ""
+    last_name = ""

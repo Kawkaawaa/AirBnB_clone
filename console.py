c5bd98b147a4aa861b4fedbaa5da001afd8aee39
Dr-Dados <adnane.bachchar@gmail.com>
2024-01-13 21:52:18 +0000

    adding all classes

--- console.py
+++ console.py
@@ -6,14 +6,24 @@ import cmd
 from models.base_model import BaseModel
 from models import storage
 from models.user import User
-from models.base_model import BaseModel
+from models.state import State
+from models.city import City
+from models.amenity import Amenity
+from models.place import Place
+from models.review import Review
 
 
 class HBNBCommand(cmd.Cmd):
     """Hbnb Command Line Interface"""
 
     prompt = '(hbnb)'
-    classes = {'BaseModel': BaseModel, 'User': User}
+    classes = {    "BaseModel": BaseModel,
+    "User": User,
+    "State": State,
+    "City": City,
+    "Amenity": Amenity,
+    "Place": Place,
+    "Review": Review}
 
     def do_quit(self, line):
         """Quit command to exit the program.
